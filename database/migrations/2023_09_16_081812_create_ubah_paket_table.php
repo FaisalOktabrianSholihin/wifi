287a61dbc6a18e0c2c0ae5c2953dd7c3d7ce1846
fawaidac <fwdachmd@gmail.com>
2023-12-24 03:32:05 +0700

    [update] table ubah paket

--- database/migrations/2023_09_16_081812_create_ubah_paket_table.php
+++ database/migrations/2023_09_16_081812_create_ubah_paket_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,8 +19,8 @@ return new class extends Migration
             $table->integer('biaya')->default(0);
             $table->integer('diskon')->default(0);
             $table->integer('bayar')->default(0);
-            $table->string('lunas', 11);
-            $table->string('keterangan', 100);
+            $table->string('lunas', 11)->nullable();
+            $table->string('keterangan', 100)->nullable();
             $table->timestamps();
         });
     }
